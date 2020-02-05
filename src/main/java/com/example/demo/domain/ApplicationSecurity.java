440fe56d5d836b0b4961313c518ab7142373b710
root <edenkang5517@gmail.com>
2020-02-06 03:08:15 +0900

    google login fix

--- src/main/java/com/example/demo/domain/ApplicationSecurity.java
+++ src/main/java/com/example/demo/domain/ApplicationSecurity.java
@@ -15,17 +15,19 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 	    {
 	        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
 	        web.ignoring().antMatchers("/css/**", "/scripts/**", "/images/**", "/fonts/**", "/plugin/**", "/vendor/**" );
-	        web.ignoring().antMatchers("/signin");
+	        web.ignoring().antMatchers("/signin**");
 	    }
     
 	   @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http.antMatcher("/**")
 	                .authorizeRequests()
-	                .antMatchers("/", "/favicon.ico", "/login**", "/signup**", "/register**", "/main**").permitAll()
+	                .antMatchers("/", "/favicon.ico", "/login**", "/signup**","/signin**", "/register**", "/main**").permitAll()
 	                .anyRequest().authenticated()
 	                .and().logout().logoutSuccessUrl("/").permitAll()
 	                .and().headers().frameOptions().sameOrigin()
 	                .and().csrf().disable();
+	                
+	       
 	    }
 }
